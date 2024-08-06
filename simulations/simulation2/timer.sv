4d4dae290a0a3bd7fd7c3c3a7da78dfb24307302
Aviv Richter <avivrichter@Avivs-MacBook-Air.local>
2024-08-06 17:31:27 +0300

    Adding timer implementation

--- simulations/simulation2/timer.sv
+++ simulations/simulation2/timer.sv
@@ -13,6 +13,26 @@ module timer(
 // Put your code here
 // ------------------
 
+    logic[4:0] counter;
+
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset == 1'b1) begin
+            counter <= 5'b0;
+        end else if (t_start == 1'b1) begin
+            counter <= 5'b0;
+            t_flicker <= 1'b0;
+        end else if (counter == t_length) begin
+            t_done <= 1'b1;
+        end else begin
+            counter <= counter + 1;
+        end
+
+        if (t_length < 5 || counter == t_length - 5) begin
+            t_flicker <= 1'b1;
+        end
+
+    end
+
 
 // End of your code
 endmodule
